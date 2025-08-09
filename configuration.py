6008aad166b3cb16e1091d7a3fc548a015b65263
Sergio Ramirez <sergio.ramirez.alvarez@gmail.com>
2025-08-09 12:03:07 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-c4a12f25-852f-4229-8d8e-771ca386cb66.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ create_user_test.py
@@ -0,0 +1,79 @@
+import sender_stand_request
+import data
+
+def get_user_body(name):
+    current_body = data.user_body.copy()
+    current_body["firstname"] = name
+    return current_body
+
+def positive_assert(first_name):
+    user_body = get_user_body(first_name)
+    user_response = sender_stand_request.post_new_user(user_body)
+    assert user_response.status_code == 201
+    assert user_response.json()["authToken"] != ""
+    user_table_response = sender_stand_request.get_users_table()
+    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
+               + user_body["address"] + ",,," + user_response.json()["authToken"]
+
+    assert user_table_response.text.count(str_user) == 1
+
+def negative_assert_symbol(first_name):
+    user_body = get_user_body(first_name)
+    response = sender_stand_request.post_new_user(user_body)
+    assert  response.status_code == 400
+    assert response.json()["code"] == 400
+    assert response.json()["message"] == "Has introducido un nombre de usuario no válido. " \
+                                         "El nombre solo puede contener letras del alfabeto latino, "\
+                                         "la longitud debe ser de 2 a 15 caracteres."
+
+def negative_assert_no_firstname(user_body):
+    response = sender_stand_request.post_new_user(user_body)
+    assert response.status_code == 400
+    assert response.json()["code"] == 400
+    assert response.json()["message"] == "No se han probado todos los parámetros requeridos"
+
+
+
+def test_create_user_2_letter_in_first_name_get_success_response():
+    positive_assert("Aa")
+
+def test_create_user_15_letter_in_first_name_get_success_response():
+    positive_assert("Aaaaaaaaaaaaaaa")
+
+def test_create_user_1_letter_in_first_name_get_error_response():
+    negative_assert_symbol("A")
+
+def test_create_user_16_letter_in_first_name_get_error_response():
+    negative_assert_symbol("Аааааааааааааааа")
+
+def test_create_user_english_letter_in_first_name_get_success_response():
+    positive_assert("QWErty")
+
+def test_create_user_has_space_in_first_name_get_error_response():
+    negative_assert_symbol("A Aaa")
+
+def test_create_user_has_special_symbol_in_first_name_get_error_response():
+    negative_assert_symbol("\"№%@\",")
+
+def test_create_user_has_number_in_first_name_get_error_response():
+    negative_assert_symbol("123")
+
+def test_create_user_no_first_name_get_error_response():
+    user_body = data.user_body.copy()
+    user_body.pop("firstName")
+    negative_assert_no_firstname(user_body)
+
+def test_create_user_empty_first_name_get_error_response():
+    user_body = get_user_body("")
+    negative_assert_no_firstname(user_body)
+
+def test_create_user_number_type_first_name_get_error_response():
+    user_body = get_user_body(12)
+    response = sender_stand_request.post_new_user(user_body)
+    assert response.status_code == 400
+
+
+
+
+
+
